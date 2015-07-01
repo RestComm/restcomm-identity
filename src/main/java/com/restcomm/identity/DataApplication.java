af3b750e84c7d344ef028ecce642dd6faa911fc2
Orestis Tsakiridis <otsakir@gmail.com>
2015-07-01 23:55:50 +0300

    Initial commit

---
+++ src/main/java/com/restcomm/identity/DataApplication.java
@@ -0,0 +1,9 @@
+package org.keycloak.example.oauth;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class DataApplication extends Application
+{
+}
