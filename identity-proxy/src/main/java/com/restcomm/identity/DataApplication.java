59c9aca99ad19acc5ec6f1ed376c6de8da568d1b
Orestis Tsakiridis <orestis.tsakiridis@telestax.com>
2016-07-19 12:23:46 +0300

    Cleared repo. A new 'theme' branch is created for 'restcomm' theme files only.
    Refers #25

--- identity-proxy/src/main/java/com/restcomm/identity/DataApplication.java
+++
@@ -1,9 +0,0 @@
-package com.restcomm.identity;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class DataApplication extends Application
-{
-}
